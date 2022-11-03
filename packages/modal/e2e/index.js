854ecacc1a16dca6ef9e729fed3ffa92a085821d
Gu-Miao <1450206741@qq.com>
2022-11-03 14:40:08 +0800

    Update test case of modal

--- packages/modal/e2e/index.js
+++ packages/modal/e2e/index.js
@@ -1,12 +1,31 @@
 /** @type {typeof import('..').default} */
 const VjsccModal = window.VjsccModal
 
-const modal = new VjsccModal({
-  el: '.vjscc-modal',
+// const modal = new VjsccModal({
+//   el: '.vjscc-modal',
+//   show: true,
+//   mask: false,
+//   maskClose: true,
+//   escapeClose: false,
+//   onConfirm() {
+//     console.log(modal)
+//   }
+// })
+
+const modal = VjsccModal.create({
   show: true,
-  maskClose: true,
+  title: 'asdfa',
+  content: 'fasdfasdfasdf',
+  footer: 'no',
+  // mask: false,
+  maskClose: false,
   escapeClose: false,
-  onConfirm() {
-    console.log(modal)
+  animation: {
+    easing(x) {
+      return x ** 1.2
+    },
+    duration: 2000
   }
 })
+
+modal.show = true
